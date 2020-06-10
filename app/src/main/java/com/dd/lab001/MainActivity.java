b3b8812a544b09f92dbad651ecb98b0f12a49b66
Divyesh <soji0002@algonquinlive.com>
2020-06-11 05:26:16 +0530

    Finished Lab3

--- app/src/main/java/com/dd/lab001/MainActivity.java
+++ app/src/main/java/com/dd/lab001/MainActivity.java
@@ -2,10 +2,16 @@ package com.dd.lab001;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.GridLayout;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -16,29 +22,61 @@ import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
+    SharedPreferences share;
+    EditText emailField;
+
     Switch switch1;
     LinearLayout linearLayout;
     GridLayout gridlayout;
     RelativeLayout relativeLayout;
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_relative);
+        setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.button);
-        switch1 = (Switch)findViewById(R.id.switch1);
-        linearLayout = (LinearLayout)findViewById(R.id.linearlayout);
-        gridlayout = (GridLayout) findViewById(R.id.gridlayout);
-        relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+//        Button button = findViewById(R.id.button);
+//        switch1 = (Switch)findViewById(R.id.switch1);
+//        linearLayout = (LinearLayout)findViewById(R.id.linearlayout);
+//        gridlayout = (GridLayout) findViewById(R.id.gridlayout);
+//        relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+//        CheckBox checkbox = (CheckBox)findViewById(R.id.checkBox);
+//        switch1.setOnCheckedChangeListener(this);
+//
+//        checkbox.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(getApplicationContext(),R.string.ButtonText, Toast.LENGTH_SHORT).show();
+//
+//            }
+//        });
+//
+//
+//        button.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                // Code here executes on main thread after user presses button
+//
+//                Toast.makeText(getApplicationContext(),R.string.toast_message, Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
-        switch1.setOnCheckedChangeListener(this);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Code here executes on main thread after user presses button
+        Button nextButton = (Button)findViewById(R.id.button);
+        emailField = (EditText)findViewById(R.id.email);
+        share = getSharedPreferences("FileName", Context.MODE_PRIVATE);
+        String saveEmail = share.getString("ReserveName", "");
+        emailField.setHint(saveEmail);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View b) {
 
-                Toast.makeText(getApplicationContext(),R.string.toast_message, Toast.LENGTH_SHORT).show();
+
+                Intent nextPage = new Intent(MainActivity.this, ProfileActivity.class);
+
+                nextPage.putExtra("emailType", emailField.getText().toString());
+                MainActivity.this.startActivityForResult(nextPage, 345);
             }
         });
+
 
     }
 
@@ -49,7 +87,7 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
                 @Override
                 public void onClick(View view) {
                     Snackbar snackbar = Snackbar
-                            .make(gridlayout, "The switch is now on", Snackbar.LENGTH_LONG)
+                            .make(linearLayout, "The switch is now on", Snackbar.LENGTH_LONG)
                             .setAction("UNDO", new View.OnClickListener() {
                         @Override
                         public void onClick(View view) {
@@ -64,7 +102,7 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
                 @Override
                 public void onClick(View view) {
                     Snackbar snackbar = Snackbar
-                            .make(gridlayout, "The switch is now off", Snackbar.LENGTH_LONG)
+                            .make(linearLayout, "The switch is now off", Snackbar.LENGTH_LONG)
                             .setAction("UNDO", new View.OnClickListener() {
                                 @Override
                                 public void onClick(View view) {
@@ -76,5 +114,14 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
                 }
             });
         }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor editor = share.edit();
+        String typeEmail  = emailField.getText().toString();
+        editor.putString("ReserveName", typeEmail);
+        editor.commit();
     }
 }
---
+++ app/src/main/java/com/dd/lab001/ProfileActivity.java
@@ -0,0 +1,79 @@
+package com.dd.lab001;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+//import android.support.annotation.Nullable;
+//import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+public class ProfileActivity extends AppCompatActivity {
+    public static final String ACTIVITY_NAME = "PROFILE_ACTIVITY";
+    ImageButton takePicture;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        //get Intent from MainActivity
+        Intent loginPage = getIntent();
+        String emailType = loginPage.getStringExtra("emailType");
+        EditText enterEmail = (EditText)findViewById(R.id.email);
+        enterEmail.setText(emailType);
+        takePicture = (ImageButton)findViewById(R.id.picture);
+        takePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View c) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(ProfileActivity.this.getPackageManager()) != null) {
+                    ProfileActivity.this.startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
+        Log.e(ACTIVITY_NAME, "In function: onCreate"  /* replace with function name */);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            takePicture.setImageBitmap(imageBitmap);
+        }
+        Log.e(ACTIVITY_NAME, "In function: onActivityResult"  /* replace with function name */);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(ACTIVITY_NAME, "In function: onStart"  /* replace with function name */);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(ACTIVITY_NAME, "In function: onResume"  /* replace with function name */);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(ACTIVITY_NAME, "In function: onPause"  /* replace with function name */);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(ACTIVITY_NAME, "In function: onStop"  /* replace with function name */);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(ACTIVITY_NAME, "In function: onDestroy"  /* replace with function name */);
+    }
+}
